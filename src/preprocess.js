baa39eed608c8b14dc5a196286a6093a6028f6e6
pschretlen <peter.schretlen@gmail.com>
2017-11-19 19:54:38 +0000

    all tests passing

--- src/preprocess.js
+++ src/preprocess.js
@@ -4,10 +4,26 @@ const fs = require('fs');
 const byline = require('byline');
 const pipe = require('pump-promise');
 const through = require('through2');
+const moment = require('moment');
 
-const process = (result) => through.obj( (obj, enc, cb) => {
+const process = (result) => through.obj( (url, enc, cb) => {
 
-	result.push(obj);
+	const latLonMatch = /maps\/@(-?\d+\.?\d*),(-?\d+\.?\d*),/g.exec(url);
+
+	const svPoint = {};
+    svPoint.lat = parseFloat( latLonMatch[1] );
+    svPoint.lon = parseFloat( latLonMatch[2] );
+    svPoint.heading = parseFloat( /(\d+\.?\d*)h/g.exec(url)[1] );
+    svPoint.pitch = parseFloat( /(\d+\.?\d*)t/g.exec(url)[1] ) - 90;
+    svPoint.fov = parseFloat( /(\d+\.?\d*)y/g.exec(url)[1] ) * 2 ;
+    svPoint.pano = /!1s([^!]*)!/g.exec(url)[1];
+
+    const date = moment( /!5s([^!]*)!/g.exec(url)[1] );
+
+    svPoint.month = date.month();
+    svPoint.year = date.year();
+
+	result.push(svPoint);
 	cb(null);
 
 });
--- test/test-preprocess.js
+++ test/test-preprocess.js
@@ -15,15 +15,15 @@ describe("Pre-Process Test", function() {
             assert.equal(92, svSequence.length);
         });
         it("should have lat/lon pairs", function() {
-            assert.equal( 90.0, svSequence[0].lat);
-            assert.equal( 90.0, svSequence[0].lon);
+            assert.equal( 43.6858598, svSequence[0].lat);
+            assert.equal( -79.3119806, svSequence[0].lon);
         });
         it("should have a panoid", function() {
-            assert.equal( 90.0, svSequence[0].panoid);
+            assert.equal( 'D517TPevWUs8JDlP2ahItA', svSequence[91].pano);
         });        
         it("should have a date (month-year)", function() {
-            assert.equal( 90.0, svSequence[0].month);
-            assert.equal( 90.0, svSequence[0].year);
+            assert.equal( 5, svSequence[0].month);
+            assert.equal( 2013, svSequence[0].year);
         });                
     });
 });
